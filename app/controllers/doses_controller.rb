ba19a0af66473c85b381b4bd977ff0115558d414
Matt Crabtree <berlintreaty@gmail.com>
2017-09-07 21:29:24 +0200

    pre-css styling

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.all
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose = @dose.cocktail
+    @dose.destroy!
+
+    redirect_to cocktails_path(@cocktail)
+  end
+
+  private
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
